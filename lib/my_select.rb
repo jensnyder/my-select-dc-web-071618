4880165aa22c6ff4de3e5ee030cc352202586792
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-22 15:22:27 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,9 +2,7 @@ def my_select(collection)
   new_collection = 0
   i = 0
   while i < collection.length
-    
-      new_collection << yield(collif yield(collection[i]) == trueection[i])
-    end
+    new_collection << yield(collection[i])
     i += 1
   end
   new_collection
